64d0389e393dddc9053131f7c2d8acef9f8f1ac7
BlaiseGratton <blaisegratton@gmail.com>
2014-12-17 16:21:25 -0600

    installed slider bar for user input

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 ;(function(){
   'use strict';
   
-  angular.module('roommateApp', ['ngRoute'])
+  angular.module('roommateApp', ['ngRoute', 'uiSlider'])
       
 }());
--- js/prof/prof.controllers.js
+++ js/prof/prof.controllers.js
@@ -55,7 +55,7 @@
       };
 
     })
-    .controller('MatchController', function(){
+    .controller('MatchController', function($scope){
       var vm = this;
     
       vm.users = [
@@ -82,18 +82,103 @@
           ques3: 4,
           ques4: 5,
           ques5: 4
-        }
+        },
+        {
+          name: "Whodat",
+          ques1: 3,
+          ques2: 1,
+          ques3: 3,
+          ques4: 2,
+          ques5: 1
+        },
+        {
+          name: "Julio",
+          ques1: 5,
+          ques2: 3,
+          ques3: 3,
+          ques4: 4,
+          ques5: 3
+        },
+        {
+          name: "Rachel",
+          ques1: 3,
+          ques2: 3,
+          ques3: 5,
+          ques4: 5,
+          ques5: 5
+        },
+        {
+          name: "Brynn",
+          ques1: 2,
+          ques2: 2,
+          ques3: 3,
+          ques4: 4,
+          ques5: 1
+        },
+        {
+          name: "Lee",
+          ques1: 1,
+          ques2: 5,
+          ques3: 1,
+          ques4: 1,
+          ques5: 5
+        },
+        {
+          name: "Martha",
+          ques1: 5,
+          ques2: 4,
+          ques3: 5,
+          ques4: 4,
+          ques5: 5
+        },
       ];
       
       vm.matches = [];
+     
+      // user defines what percent error registers as a match between two responses
+      vm.tightness = 0;
+
+      $scope.$watch("matchCtrl.tightness", function(newVal, oldVal){
+        vm.findMatches(0);
+      }, true);
 
 
-      // user defines what percent error indicates a match between two responses
-      vm.tightness = .75;
+      
+      vm.findMatches = function(userId){
+        vm.matches = [];
+        var user = vm.users[userId];
+        for(var i = 0; i < vm.users.length; i++){
+          if (i !== userId) {
+            var quesDiff = [];
+            quesDiff.push(Math.abs((user.ques1 - vm.users[i].ques1) / 5));
+            quesDiff.push(Math.abs((user.ques2 - vm.users[i].ques2) / 5));
+            quesDiff.push(Math.abs((user.ques3 - vm.users[i].ques3) / 5));
+            quesDiff.push(Math.abs((user.ques4 - vm.users[i].ques4) / 5));
+            quesDiff.push(Math.abs((user.ques5 - vm.users[i].ques5) / 5));
+            console.log(quesDiff);
 
-      function findMatches(){
-        
-      }
+            var avgDiff = (quesDiff.reduce(function(prev, cur) {
+              return prev + cur;
+            })) / 5;
+
+            if (avgDiff <= vm.tightness/100) {
+              console.log(vm.users[i].name);
+              vm.matches.push(vm.users[i]);
+            }
+
+            console.log(user.name + " & " + vm.users[i].name);
+            console.log(Math.abs((user.ques1 - vm.users[i].ques1) / 5));
+            console.log(Math.abs((user.ques2 - vm.users[i].ques2) / 5));
+            console.log(Math.abs((user.ques3 - vm.users[i].ques3) / 5));
+            console.log(Math.abs((user.ques4 - vm.users[i].ques4) / 5));
+            console.log(Math.abs((user.ques5 - vm.users[i].ques5) / 5));
+            
+          }
+        }
+      };
+
+      vm.findMatches(0);
+
     })
     .controller('EditController', function(){
       var vm = this;
